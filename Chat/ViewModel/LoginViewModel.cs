700090bfe9a6b32dd4a2093092751c82f41f689c
Пользователь <spaces_dima@mail.ru>
2021-07-03 16:07:24 +0600

    Add view mode login and register.

---
+++ Chat/ViewModel/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Chat.ViewModel
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Login")]
+        public string Login { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+    }
+}
---
+++ Chat/ViewModel/RegisterViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Chat.ViewModel
+{
+    public class RegisterViewModel: LoginViewModel
+    {
+
+        [Required]
+        [Compare("Password", ErrorMessage = "Пароли не совпадают")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Подтвердить пароль")]
+        public string PasswordConfirm { get; set; }
+    }
+}
